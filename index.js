982eeac8dfc2819d92df81d47033244b11ee407d
hamzasahi <57505833+hamzasahi@users.noreply.github.com>
2020-08-21 15:36:53 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,79 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+const Settings = global.Settings = require("./Settings/Settings.json");
+
+console.log("Launching bot...");
+let _client = new Discord.Client();
+if (Settings.Private_Server === true) {
+    _client = new Discord.Client({
+        fetchAllMembers: true
+    });
+}
+const client = global.client = _client;
+const Commands = global.Commands = new Map();
+
+console.log("--------------------------------");
+console.log("Loading commands...");
+fs.readdirSync("./Commands", { encoding: "utf-8" }).filter(file => file.endsWith(".js")).forEach(file => {
+    let prop = require(`./Commands/${file}`);
+    if (prop.conf.commands == undefined || prop.run == undefined) return console.error(`[COMMAND] ${file} is not load.`);
+    if (prop.conf.commands && prop.conf.commands.length > 0) {
+        prop.conf.commands.forEach(aliase => Commands.set(aliase, prop));
+    }
+    if (prop.onLoad != undefined && typeof (prop.onLoad) == "function") prop.onLoad(client);
+    console.log(`[COMMAND] A total of ${prop.conf.commands.length} supporters have been installed for ${file}.`);
+});
+console.log("--------------------------------");
+console.log("Loading events...");
+fs.readdirSync("./Events", { encoding: "utf-8" }).filter(file => file.endsWith(".js")).forEach(file => {
+    let prop = require(`./Events/${file}`);
+    client.on(prop.conf.event, prop.execute);
+    console.log(`[EVENT] ${file} is loaded.`);
+});
+
+console.log("--------------------------------");
+console.log("| Preparation has been completed. Starting the bot now |");
+
+require("./bot.js");
+
+/*
+
+MIT License
+
+
+
+Copyright (c) 2020 Serendia Squad
+
+
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+
+of this software and associated documentation files (the "Software"), to deal
+
+in the Software without restriction, including without limitation the rights
+
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+
+copies of the Software, and to permit persons to whom the Software is
+
+furnished to do so, subject to the following conditions:
+
+
+
+The above copyright notice and this permission notice shall be included in all
+
+copies or substantial portions of the Software.
+
+
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+
+OUT OF OR IN */
